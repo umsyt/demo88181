5b1b3da95b6d106419e39ae15ff42c84aa5018bc
umsyt <mxcheeks@gmail.com>
2021-03-07 00:03:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function bruh(){
+  var p = document.getElementById("q").value;
+  window.location.href = (window.location.href+"/../"+p+".html");
+}
+
+function amogus(){
+  var p = document.getElementById("f").value;
+  window.location.href = (window.location.href+"/../"+p+"/");
+}
+
+function vent(){
+  var p = document.getElementById("f").value; 
+  window.location.href = (window.location.href+"/../../"+p+"/");
+}
